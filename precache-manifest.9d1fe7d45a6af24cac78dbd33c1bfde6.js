a129b06bcd69e01249d12f7f63bad64c7417fc6b
ivanlalic <ivanlalic@gmail.com>
2020-08-12 02:23:47 -0300

    Updates

---
+++ precache-manifest.9d1fe7d45a6af24cac78dbd33c1bfde6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e6be4693337d1d57d23d6a090e8b40",
+    "url": "/newswebsite/index.html"
+  },
+  {
+    "revision": "526960379943e7060472",
+    "url": "/newswebsite/static/css/main.f16f4e39.chunk.css"
+  },
+  {
+    "revision": "dcc047b19732044af71a",
+    "url": "/newswebsite/static/js/2.6a55a079.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newswebsite/static/js/2.6a55a079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526960379943e7060472",
+    "url": "/newswebsite/static/js/main.9745e642.chunk.js"
+  },
+  {
+    "revision": "5479d532c7d8cbadb3f6",
+    "url": "/newswebsite/static/js/runtime-main.1594035c.js"
+  }
+]);
